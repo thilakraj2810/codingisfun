4b5535cd834f64ebeee822dee82d146d94f4a094
thilakraj2810 <33540024+thilakraj2810@users.noreply.github.com>
2021-08-19 13:16:11 +0530

    first commit

---
+++ src/main/java/com/learn/rw/RefreshRestFulServicesApplication.java
@@ -0,0 +1,13 @@
+package com.learn.rw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshRestFulServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefreshRestFulServicesApplication.class, args);
+	}
+
+}
